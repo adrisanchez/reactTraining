8e604565d728cf4a0503cae7ad737fb3f92e98e0
ES\asanchezrecuero <asanchezrecuero@deloitte.es>
2024-09-09 14:51:16 +0200

    New modifications and features from React used

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
+
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept/CoreConcept";
 import TabButton from "./components/TabButton/TabButton";
 
 import componentImg from "./assets/components.png";
-import { CORE_CONCEPTS } from "./data";
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
 import "./App.css";
 
-
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState<
+    keyof typeof EXAMPLES | ""
+  >("");
+
+  function handleSelect(selectedButton: keyof typeof EXAMPLES) {
+    setSelectedTopic(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -15,7 +24,10 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
+            {/* <CoreConcept
               title="Components"
               description="The Core UI building block."
               image={componentImg}
@@ -26,21 +38,49 @@ function App() {
               image={CORE_CONCEPTS[1].image}
             />
             <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} /> */}
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton
+              onSelect={() => handleSelect("components")}
+              isSelected={selectedTopic === "components"}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              onSelect={() => handleSelect("jsx")}
+              isSelected={selectedTopic === "jsx"}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              onSelect={() => handleSelect("props")}
+              isSelected={selectedTopic === "props"}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              onSelect={() => handleSelect("state")}
+              isSelected={selectedTopic === "state"}
+            >
+              State
+            </TabButton>
           </menu>
+          {selectedTopic && (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
         </section>
       </main>
     </div>
   );
 }
-
 export default App;
--- src/components/TabButton/TabButton.tsx
+++ src/components/TabButton/TabButton.tsx
@@ -1,9 +1,11 @@
+import { MouseEventHandler } from "react";
 import "./TabButton.css";
 
-function TabButton(props: Readonly<{ children: React.ReactNode }>) {
+function TabButton(props: Readonly<{ children: React.ReactNode, onSelect: MouseEventHandler<HTMLButtonElement> | undefined, isSelected: boolean }>) {
+
   return (
     <li>
-      <button>{props.children}</button>
+      <button className={props.isSelected ? "active" : undefined} onClick={props.onSelect}>{props.children}</button>
     </li>
   );
 }
